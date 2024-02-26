cfd2616ccce9511cc867c7f1d792652ba9d49d7b
JustineXp <justineislearning@gmail.com>
2024-02-26 19:05:53 +0300

    Added Methods to Deck class

--- DeckClass.py
+++ DeckClass.py
@@ -5,6 +5,8 @@ class Deck:
 
     def __init__(self, cards):
         self.cards = cards
+        self.shuffled_cards = []
+        self.shuffle()
 
     def __repr__(self):
         print([f'{card["name"]} of {card["type"]}' for card in self.cards])
@@ -12,7 +14,7 @@ class Deck:
     def shuffle(self):
         shuffled_cards = self.cards[:]
         shuffle(shuffled_cards)
-        return shuffled_cards
+        self.shuffled_cards = shuffled_cards
 
     def length(self):
         return len(self.cards)
@@ -37,3 +39,36 @@ class Deck:
         grouped_possible_cards = {
             'same_name': [card for card in possible_cards if played_card['name'] == card['name']],
             'same_type': [card for card in possible_cards if played_card['type'] == card['type']]}
+
+    # CREATION OF PLAYER DECKS
+    @classmethod
+    def decks_creation(cls, number_of_players):
+        player_decks = []
+        rotation_list = list(range(1, number_of_players+1))
+        shuffle(rotation_list)
+
+        for player in range(number_of_players):
+            player_decks.append(
+                {'player': player, 'deck': [], 'rotation_number': rotation_list[player]})
+
+        print(player_decks)
+
+        return player_decks
+
+    # GENERATE CARDS FOR A PLAYER
+    def player_cards_assigner(self, shuffled_cards, number_of_players, player_decks, no_of_cards):
+        number_of_cards = number_of_players * no_of_cards
+        shuffled_cards_copy = self.shuffled_cards[:]
+        for deck in player_decks:
+            deck['deck'] = shuffled_cards_copy[deck['player']:number_of_cards:number_of_players]
+
+        remaining_deck = shuffled_cards[number_of_cards:]
+
+        for deck in player_decks:
+            print('PLAYER : ' + str(deck['player']+1))
+            print('*'*30)
+
+            for card in deck['deck']:
+                print(card['name'], ' : ', card['type'].title())
+            print()
+        return player_decks, remaining_deck
