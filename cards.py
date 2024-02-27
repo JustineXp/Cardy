6b59b407a1c759552225627540593e640c5a6a3e
JustineXp <justineislearning@gmail.com>
2024-02-27 14:11:18 +0300

    Refactored the Deck Creation in the Deck Class

--- cards.py
+++ cards.py
@@ -7,25 +7,29 @@ from player import Player
 
 
 def main():
-    deck = Deck(cards)
-    print(deck.length())
-    shuffled_cards = deck.shuffled_cards
     players, no_of_cards = number_of_players()
-    empty_player_decks = deck.decks_creation(players)
-    player_decks, remaining_deck = deck.player_cards_assigner(
-        shuffled_cards, players, empty_player_decks, no_of_cards)
-    # picking_cards, starting_card = starting_card_generator(remaining_deck)
-    starting_card, picking_cards = deck.starting_card(remaining_deck)
-    #
+    deck = Deck(cards, players, no_of_cards)
+    player_decks = deck.player_decks
+    remaining_deck = deck.remaining_cards
+    starting_card = deck.the_starting_card
+    picking_cards = deck.remaining_cards
 
-    # print(player)
     print('PICKING CARDS')
     print(len(picking_cards))
 
+    print(player_decks)
+    for deck in player_decks:
+        print('PLAYER : ' + str(deck['player']+1))
+        print('*'*30)
+
+        for card in deck['deck']:
+            print(card['name'], ' : ', card['type'].title())
+        print()
+
     print('STARTING CARD')
     print(f'{starting_card["name"]} : {starting_card["type"]}')
-    playing_player = who_is_to_play(1, player_decks)
-    next_play(starting_card, picking_cards, playing_player)
+
+    print([card for card in remaining_deck])
 
 
 def card_picking(remaining_deck, player_decks, number_to_pick, player):
@@ -36,30 +40,30 @@ def card_picking(remaining_deck, player_decks, number_to_pick, player):
                 deck['deck'].extend(picked_card)
 
 
-def who_is_to_play(player_number, player_decks):
-    playing_player = [
-        player for player in player_decks if player['rotation_number'] == player_number]
-    print(playing_player[0]['deck'])
-    return playing_player[0]
+# def who_is_to_play(player_number, player_decks):
+#     playing_player = [
+#         player for player in player_decks if player['rotation_number'] == player_number]
+#     print(playing_player[0]['deck'])
+#     return playing_player[0]
 
 
-def next_play(previous_card, picking_cards, playing_player):
-    similar_name_cards = list(filter(
-        lambda card: card['name'] == previous_card['name'], playing_player['deck']))
-    similar_type_cards = list(filter(
-        lambda card: card['type'] == previous_card['type'], playing_player['deck']))
+# def next_play(previous_card, picking_cards, playing_player):
+#     similar_name_cards = list(filter(
+#         lambda card: card['name'] == previous_card['name'], playing_player['deck']))
+#     similar_type_cards = list(filter(
+#         lambda card: card['type'] == previous_card['type'], playing_player['deck']))
 
-    # print(similar_name_cards)
-    possible_cards = similar_name_cards + similar_type_cards
-    print(possible_cards)
-    if len(possible_cards) > 0:
-        card_to_play = choice(possible_cards)
-        print(card_to_play)
-        return card_to_play
-    else:
-        picked_card = picking_cards[0]
-        picking_cards = picking_cards[1:]
-        return picked_card, picking_cards
+#     # print(similar_name_cards)
+#     possible_cards = similar_name_cards + similar_type_cards
+#     print(possible_cards)
+#     if len(possible_cards) > 0:
+#         card_to_play = choice(possible_cards)
+#         print(card_to_play)
+#         return card_to_play
+#     else:
+#         picked_card = picking_cards[0]
+#         picking_cards = picking_cards[1:]
+#         return picked_card, picking_cards
 
 # How many players are playing the game
 
